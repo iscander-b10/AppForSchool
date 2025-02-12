20dbca01e75c98b7d1ef531e049abb8095bce77d
iscander-b10 <alexbolbychev@gmail.com>
2025-02-13 00:05:40 +0300

    id, full name, subjects, grades are added and generated to the generateStudent object

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { faker } from "@faker-js/faker/locale/ru"
+
+const subjects = ["Русский язык", "Математика", "Литература", "Иностранный язык", "История", "Информатика", "Физкультура"];
+const generateStudent = () => ({
+  id : faker.string.uuid(),
+  fullName : `${faker.person.lastName()} ${faker.person.firstName()} ${faker.person.middleName()}`,
+  subjects : subjects.reduce((acc, subject) => {
+    acc[subject] = Array.from({ length: 4 }, () => Math.floor(Math.random() * 4) + 2);
+    return acc;
+  }, {})
+})
+
+
+const students = Array.from({ length: 10 }, generateStudent);
+console.log("students",students)
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      hello world
+    </div>
   )
 }
 
-export default App
+export default App;

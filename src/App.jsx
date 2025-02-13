9a40626271eb9e02542c7a974a79cde7ffc72cc9
iscander-b10 <alexbolbychev@gmail.com>
2025-02-13 23:00:09 +0300

    Ann array with student classes has been henerated

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,22 @@ const generateStudent = () => ({
   }, {})
 })
 
+const generateClasses = () => {
+  const classes = [];
+
+  for ( let number = 1; number <= 11; number++) {
+    ["А", "Б", "В"].forEach((symbol) => {
+      const className = `${number}${symbol}`;
+      classes.push({
+        name : className
+      });
+    });
+  }
+  return classes;
+}
+
+const allClasses = generateClasses();
+console.log("allClasses",allClasses);
 
 const students = Array.from({ length: 10 }, generateStudent);
 console.log("students",students)

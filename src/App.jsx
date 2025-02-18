e9e145f58941446f8a68cc3e748132cac95d69d5
iscander-b10 <alexbolbychev@gmail.com>
2025-02-18 22:50:10 +0300

    the prepareTableData function has been added, an array of students has been output to the table component

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,8 @@
-import { faker } from "@faker-js/faker/locale/ru"
-
-const subjects = ["Русский язык", "Математика", "Литература", "Иностранный язык", "История", "Информатика", "Физкультура"];
-const generateStudent = () => ({
-  id : faker.string.uuid(),
-  fullName : `${faker.person.lastName()} ${faker.person.firstName()} ${faker.person.middleName()}`,
-  subjects : subjects.reduce((acc, subject) => {
-    acc[subject] = Array.from({ length: 4 }, () => Math.floor(Math.random() * 4) + 2);
-    return acc;
-  }, {})
-})
-
-const generateClasses = () => {
-  const classes = [];
-
-  for ( let number = 1; number <= 11; number++) {
-    ["А", "Б", "В"].forEach((symbol) => {
-      const className = `${number}${symbol}`;
-      classes.push({
-        name : className
-      });
-    });
-  }
-  return classes;
-}
-
-const allClasses = generateClasses();
-console.log("allClasses",allClasses);
-
-const students = Array.from({ length: 10 }, generateStudent);
-console.log("students",students)
+import StudentTable from "./assets/StudentTable/StudentTable.jsx"
 
 function App() {
   return (
-    <div>
-      hello world
-    </div>
+    <StudentTable></StudentTable>
   )
 }
 
---
+++ src/assets/StudentTable/StudentTable.jsx
@@ -0,0 +1,104 @@
+import { ConfigProvider } from "antd"
+import ru_RU from "antd/locale/ru_RU"
+import { faker } from "@faker-js/faker/locale/ru"
+import { Table } from "antd";
+
+const subjects = ["Русский язык", "Математика", "Литература", "Иностранный язык", "История", "Информатика", "Физкультура"];
+
+const generateStudent = (className) => {
+  const grades = subjects.reduce((acc, assessment ) => {
+    acc[assessment] = Array.from({ length: 4 }, () => Math.floor(Math.random() * 4) + 2);
+    return acc;
+  }, {});
+  
+  const avgGrades = Object.entries(grades).reduce((acc, [subject, marks]) => {
+    acc[subject] = marks.reduce((sum, mark) => sum + mark, 0) / marks.length;
+    return acc;
+  }, {})
+
+  return{
+    id: faker.string.uuid(),
+    fullName: `${faker.person.lastName()} ${faker.person.firstName()} ${faker.person.middleName()}`,
+    className,
+    grades,
+    avgGrades
+  }
+}
+
+const generateClasses = () => {
+  const classes = [];
+
+  for ( let number = 1; number <= 11; number++) {
+    ["А", "Б", "В"].forEach((symbol) => {
+      const className = `${number}${symbol}`;
+      const students = Array.from({ length : 15 }, () => generateStudent(className));
+      classes.push({
+        className,
+        students
+      });
+    });
+  }
+  return classes;
+}
+
+//Преобразование данных для таблицы
+const prepareTableData = (classes) => {
+  return classes.flatMap(classItem => 
+    classItem.students.map(student => ({
+      ...student,
+      key: student.id,
+      grades: Object.entries(student.grades).map(([subject, grades]) => ({
+        subject,
+        grades: grades.join(', '),
+        avg: student.avgGrades[subject]
+      }))
+    })
+  ))
+};
+
+function StudentTable () {
+  const classes = generateClasses();
+  const tableData = prepareTableData(classes);
+
+  const columns = [
+    {
+      title: "Класс",
+      dataIndex: "className",
+      key: "className",
+      filters: Array.from(new Set(tableData.map(item => item.className))).map(c => ({text: c, value: c})),
+      onFilter: (value, record) => record.className === value,
+    },
+    {
+      title: "ФИО",
+      dataIndex: "fullName",
+      key: "fullName",
+      sorter: (a, b) => a.fullName.localeCompare(b.fullName)
+    },
+    {
+      title: "Оценки",
+      dataIndex: "grades",
+      key: "grades",
+      render: (grades) => (
+        <div>
+          {grades.map((item, index) => (
+            <div key={index}>
+              {item.subject}: {item.grades} (Средний: {item.avg})
+            </div>
+          ))}
+        </div>
+      ),
+    }
+  ]
+    return(
+      <ConfigProvider locale={ru_RU}>
+        <Table
+          columns={columns}
+          dataSource={tableData}
+          pagination={{ pageSize: 45 }}
+          rowKey="key"
+        />
+      </ConfigProvider>
+    )
+}
+
+export default StudentTable;
